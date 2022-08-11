5d115aa743082e386fba28eb99bfd2674c017070
MassiGy <ghernaoutmassi@gmail.com>
2022-08-11 20:04:22 +0200

    like post controllers created + linked to the routes

---
+++ src/controllers/Post/likePost.js
@@ -0,0 +1,51 @@
+
+/**
+ * @module Like Post Controller
+ * 
+ * @param {Request} req - HTTP Request from the client
+ * @param {Response} res - HTTP Response for the client
+ * 
+ * @description
+ * This controller will allow the user to like a post, if all parameters are correct.
+ * 
+ * @todo
+ * Nothing for now.
+ */
+
+
+
+
+ module.exports.likePost = async (req, res) => {
+
+    if (!req.params.postId) {
+        return res.status(400).send('invalid post id.');
+    }
+
+    // find the original post.
+
+    const foundPost = await Post.findOne({
+        where: {
+            id: req.params.postId,
+        }
+    });
+
+    // check that it exists.
+
+    if (!foundPost) {
+        return res.status(404).send({ msg: 'The post that you are trying to update does not exist.' });
+    }
+
+    // check if the owner is the active user.
+
+   
+    // push the user id to the post likers array.
+
+    // foundPost.likedBy.push(req.user.id)
+    foundPost.likedBy.push(1);
+
+
+    // inform the user with the process status.
+
+    return res.status(200).send('post liked!');
+}
+
